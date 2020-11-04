b311707f1482bf1aa0ae36b6d6011855e7e4fd56
laeeqah <laeeqahesau@gmail.com>
2020-11-04 15:31:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+temp = np.array([220, 330, 190, 340, 410, 445, 415])
+soup_sale = np.array([14.2,16.5,11.8,15.3,18.8,22.5,19.5])
+
+plt.scatter(soup_sale, temp)
+
+plt.plot( soup_sale,temp,'o')
+m, b = np.polyfit( soup_sale,temp, 1)
+plt.plot(soup_sale, m * soup_sale + b, color= "b")
+plt.xlabel([12.5,22.5])
+plt.ylabel([200,450])
+
+plt.title("Soup sales in relation to temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Price in (R)")
+
+plt.show()
